c1c9af39b0570f787e64ee9d20e3d445495ce295
Stepan <kurakste@gmail.com>
2019-01-03 21:51:33 +0300

    Fix project structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, BrowserRouter } from 'react-router-dom'; 
+import Layout from './components/Layout';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Layout>
+            <Switch>
+              <Route exact path='/' render={ () => { return (<div> Hi from root </div>)}} />
+              <Route exact path='/about' render={ () => { return (<div> Hi from about </div>)}} />
+              <Route exact path='/catalog' render={ () => { return (<div> Hi from catalog </div>)}} />
+            
+            </Switch>
+          </Layout >
+        </div>
+      </BrowserRouter>
     );
   }
 }
