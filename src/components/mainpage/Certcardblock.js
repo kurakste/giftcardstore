5f16a0663ecb64dd1d1e080b938795fa16dbad9f
Stepan <kurakste@gmail.com>
2019-01-04 10:59:31 +0300

    Add cert id field

--- src/components/mainpage/Certcardblock.js
+++ src/components/mainpage/Certcardblock.js
@@ -1,15 +1,17 @@
 import React, { Component, Fragment } from 'react';
 import {
-    Paper, Typography, Button 
+    Paper, Typography, Button, TextField 
   } from '@material-ui/core';
 
 class Certcardblock extends Component {
-
   render() {
+    const typeBodyStyle = {
+      width: '90%', marginLeft: '5%', marginTop: '3%', marginBottom: '3%'
+    };
     return (
       <Fragment>
         <Paper
-          style = {{textAlign: 'center'}}
+          style = {{textAlign: 'center', paddingTop: '5%', paddingBottom: '5%'}}
         >
           <Typography
             align = 'center'
@@ -22,12 +24,11 @@ class Certcardblock extends Component {
           align = 'justify'
           color = 'primary'
           variant = 'body1'
-          style = {{
-            width: '90%', marginLeft: '5%', marginTop: '3%', marginBottom: '3%'
-          }}
+          style = {typeBodyStyle}
           >
           Вам подарили похожий сертификат?
-            Мы рады что нам доверили Вас поздравить. Сделаем все что бы вы получили удовольствие от вашего подарка. Введите пожалуйста код сертификата.
+            Мы рады что нам доверили Вас поздравить. Сделаем все что бы вы получили удовольствие от вашего подарка. 
+            Введите пожалуйста код сертификата.
             </Typography>
           <img 
             style = {{ 
@@ -35,14 +36,31 @@ class Certcardblock extends Component {
 
             src = './img/vaucher-exp.png' 
           />
-
+            
           <Typography
           align = 'justify'
           color = 'primary'
           variant = 'body1'
-          style = {{
-            width: '90%', marginLeft: '5%', marginTop: '3%', marginBottom: '3%'
-          }}
+          style = {typeBodyStyle}
+          >
+          
+          </Typography>
+
+         <TextField
+            id="standard-full-width"
+            label="Введите пожалуйста код сертификата:"
+            style={{ margin: 8, width: '80%' }}
+            placeholder="XXXX-XXXXXX"
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+        />
+          <Typography
+          align = 'justify'
+          color = 'primary'
+          variant = 'body1'
+          style = {typeBodyStyle}
           >
 
             Кнопка "Уточнить" переведет вас на страницу с подробным описанием товара. Кнопка "активировать" - активация подарка. Нажмите ее и мы свяжимся для уточнения деталей организации вашего мероприятия.
