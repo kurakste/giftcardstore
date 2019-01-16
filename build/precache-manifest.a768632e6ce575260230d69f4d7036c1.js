b55264f8d4334f0d21a610860a31fdea4e5f18ff
Stepan <kurakste@gmail.com>
2019-01-16 20:50:34 +0300

    Add production build

---
+++ build/precache-manifest.a768632e6ce575260230d69f4d7036c1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f511f105d3929a15b9d",
+    "url": "/static/js/main.9f511f10.chunk.js"
+  },
+  {
+    "revision": "7ff028913c0f41ce7a8a",
+    "url": "/static/js/1.7ff02891.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2259ea7c966837a475fc852709af2aee",
+    "url": "/index.html"
+  }
+];
